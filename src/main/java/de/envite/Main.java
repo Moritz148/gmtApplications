f689b73d2f07ced57c1cd6e254860724e407050b
MoritzSchwarz <moritz.schwarz148@gmx.de>
2025-09-30 12:25:48 +0200

    test some new application.yaml features

--- src/main/java/de/envite/Main.java
+++ src/main/java/de/envite/Main.java
@@ -1,6 +1,8 @@
 package de.envite;
 
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.ZeebeFuture;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 import io.camunda.zeebe.client.api.response.Topology;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,9 +24,11 @@ public class Main implements CommandLineRunner {
 
         private ZeebeClient zeebeClient;
 
-    @Value("${process.instances:100}")
-    private int processInstances;
+    @Value("${experiment.process-instances.amount}")
+    private int numberOfInstances;
 
+    @Value("${experiment.process-name}")
+    private String processName;
 
     public Main(ZeebeClient zeebeClient) {
         this.zeebeClient = zeebeClient;
@@ -37,21 +41,34 @@ public class Main implements CommandLineRunner {
             return;
         }
 
-        int numberOfInstances = processInstances;
-
         getTopology();
 
-        String processClasspath = "C8_single" + ".bpmn";
+        String processClasspath = processName + ".bpmn";
 
         deployBPMN(processClasspath);
 
-        String processId = "C8_single";
+//        String processId = "C8_single";
 
-        for (int i = 1; i <= numberOfInstances; i++) {
+//        for (int i = 1; i <= numberOfInstances; i++) {
+//
+//            startInstance(processName, i);
+//
+//        }
+        int instancesCreating = 1;
 
-            startInstance(processId, i);
+        Instant start = Instant.now();
+        long startMicros = getMicros(start);
+        System.out.println("Instance #" + instancesCreating + " STARTED - " + startMicros);
 
+        while (instancesCreating <= numberOfInstances){
+            startInstance(processName, instancesCreating);
+            instancesCreating++;
         }
+
+        Instant end = Instant.now();
+        long endMicros = getMicros(end);
+        System.out.println("Instance #" + instancesCreating + " ENDED - " + endMicros);
+
     }
 
     private void getTopology(){
@@ -76,25 +93,31 @@ public class Main implements CommandLineRunner {
         System.out.printf("Deployed BPMN %s\n", classpath);
     }
 
-    private void startInstance(String processId, int instance){
+    private void startInstance(String id, int instance){
 
-        if(instance==1){
-            Instant start = Instant.now();
-            long startMicros = getMicros(start);
-            System.out.println("Instance #" + instance + " STARTED - " + startMicros);
-        }
+//        if(instance==1){
+//            Instant start = Instant.now();
+//            long startMicros = getMicros(start);
+//            System.out.println("Instance #" + instance + " STARTED - " + startMicros);
+//        }
 
-        zeebeClient.newCreateInstanceCommand()
-                .bpmnProcessId(processId)
-                .latestVersion()
-                .send()
-                .join();
+        ZeebeFuture<ProcessInstanceEvent> future =
+                zeebeClient.newCreateInstanceCommand()
+                        .bpmnProcessId(id)
+                        .latestVersion()
+                        .send();
 
-        if(instance==processInstances){
-            Instant end = Instant.now();
-            long endMicros = getMicros(end);
-            System.out.println("Instance #" + instance + " ENDED - " + endMicros);
-        }
+//        zeebeClient.newCreateInstanceCommand()
+//                .bpmnProcessId(processId)
+//                .latestVersion()
+//                .send()
+//                .join();
+
+//        if(instance==numberOfInstances){
+//            Instant end = Instant.now();
+//            long endMicros = getMicros(end);
+//            System.out.println("Instance #" + instance + " ENDED - " + endMicros);
+//        }
     }
 
     private static long getMicros(Instant time) {
