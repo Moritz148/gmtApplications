b5d67fff60cb3701cf0258a9f2cc6426e0fb4e0c
MoritzSchwarz <moritz.schwarz148@gmx.de>
2025-10-27 10:20:20 +0100

    GMT Ready

--- src/main/java/de/envite/Main.java
+++ src/main/java/de/envite/Main.java
@@ -5,6 +5,7 @@ import io.camunda.client.annotation.Deployment;
 import io.camunda.client.api.response.ProcessInstanceEvent;
 import io.camunda.client.api.response.Topology;
 import io.camunda.client.api.search.enums.ProcessInstanceState;
+import io.grpc.internal.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -15,9 +16,8 @@ import java.io.IOException;
 import java.time.Instant;
 
 @SpringBootApplication
-@Deployment(resources = "classpath:C8_benchmark.bpmn")
 public class Main implements CommandLineRunner {
-    //    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class, args);
         context.close();
@@ -26,9 +26,13 @@ public class Main implements CommandLineRunner {
     @Autowired
     private CamundaClient client;
 
-
-    @Value("${mo.may-fancy-namespace.my-vlue}")
+    @Value("${process-instances}")
     int amountProcessInstances;
+
+
+    String processId = "C8_benchmark";
+
+    String processClasspath = processId + ".bpmn";
 
     @Override
     public void run(String... args) throws Exception {
@@ -36,17 +40,15 @@ public class Main implements CommandLineRunner {
             System.out.println("Camunda Client not set");
             return;
         }
+        System.out.println("Starting " + amountProcessInstances + " Process Instances");
 
         getTopology();
 
+        deployBPMN(processClasspath);
+
         Thread.sleep(5000);
         for (int i = 1; i <= amountProcessInstances; i++) {
-            ProcessInstanceEvent processInstanceEvent = client.newCreateInstanceCommand()
-                    .bpmnProcessId("C8_benchmark")
-                    .latestVersion()
-                    .send()
-                    .join();
-//            long process_instance_key = processInstanceEvent.getProcessInstanceKey();
+            startInstance(processId);
 
             if(i==1){
                 Instant start = Instant.now();
@@ -54,6 +56,10 @@ public class Main implements CommandLineRunner {
                 System.out.println("Instance #" + i + " STARTED - " + startMicros);
             }
         }
+        checkInstances();
+    }
+
+    private void checkInstances() throws InterruptedException {
         boolean x = true;
         while (x) {
             var response = client.newProcessInstanceSearchRequest().filter((f) -> f.processDefinitionId("C8_benchmark").state(ProcessInstanceState.COMPLETED))
@@ -76,8 +82,8 @@ public class Main implements CommandLineRunner {
     }
 
     private void getTopology() throws IOException {
+        Topology topology = client.newTopologyRequest().send().join();
 
-        final Topology topology = client.newTopologyRequest().send().join();
 
         System.out.println("Topology: ");
         topology.getBrokers()
@@ -87,42 +93,26 @@ public class Main implements CommandLineRunner {
                             .forEach(p -> System.out.println("     " + p.getPartitionId() + " - " + p.getRole()));
                 });
         System.out.println();
-
     }
 
-    /*private void deployBPMN(String classpath){
-        zeebeClient.getConfiguration();
-        zeebeClient.newDeployResourceCommand()
+    private void deployBPMN(String classpath){
+        client.getConfiguration();
+        client.newDeployResourceCommand()
                 .addResourceFromClasspath(classpath)
                 .send()
                 .join();
         System.out.printf("Deployed BPMN %s\n", classpath);
     }
 
-    private void startInstance(String processId, int instance){
-
-        if(instance==1){
-            Instant start = Instant.now();
-            long startMicros = getMicros(start);
-            System.out.println("Instance #" + instance + " STARTED - " + startMicros);
-        }
-
-        zeebeClient.newCreateInstanceCommand()
-                .bpmnProcessId(processId)
-                .latestVersion()
-                .withResult()
-                .send()
-                .join();
-
-        if(instance==processInstances){
-            Instant end = Instant.now();
-            long endMicros = getMicros(end);
-            System.out.println("Instance #" + instance + " ENDED - " + endMicros);
-        }
-    }*/
+    private void startInstance(String processId){
+        client.newCreateInstanceCommand()
+                    .bpmnProcessId(processId)
+                    .latestVersion()
+                    .send()
+                    .join();
+    }
 
     private static long getMicros(Instant time) {
         return time.getEpochSecond() * 1_000_000L + time.getNano() / 1_000;
     }
-
 }
